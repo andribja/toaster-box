763988d6f5153e29485938eb0b049191f9290e09
andribja <andribja@gmail.com>
2015-05-29 14:58:46 -0700

    flashlight working, 2 scenes

---
+++ Assets/flashlight/TurnOnFlashlight.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurnOnFlashlight : MonoBehaviour {
+	private Light lt;
+	// Use this for initialization
+	void Start () {
+		lt = GetComponent<Light>();
+		lt.intensity = 0;
+	}
+
+	public void turnOn() {
+		lt.intensity = 5;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
