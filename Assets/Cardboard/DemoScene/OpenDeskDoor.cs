7b0a4534c6bfada4fc17847c793ea61d7876924b
andribja <andribja@gmail.com>
2015-05-29 16:19:29 -0700

    doors working, battery added

--- Assets/Cardboard/DemoScene/OpenDeskDoor.cs
+++ Assets/Cardboard/DemoScene/OpenDeskDoor.cs
@@ -1,11 +1,13 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class OpenDeskDoor : MonoBehaviour {
 	private Vector3 startingPosition;
+	private float openSign = 0;
 	private bool isOpening = false;
-	private float timer = 0;
-	private int isOpen = 1; // 1 = closed -1 = open
+	private bool isOpen = false;
+	private float currentRotation = 0;
+	private float rightDoor = 1;
 	
 	void Start() {
 		startingPosition = transform.localPosition;
@@ -13,7 +15,7 @@ public class OpenDeskDoor : MonoBehaviour {
 	}
 	
 	public void SetGazedAt(bool gazedAt) {
-		GetComponent<Renderer>().material.color = gazedAt ? Color.green : Color.red;
+		//GetComponent<Renderer>().material.color = gazedAt ? Color.green : Color.red;
 	}
 	
 	public void Reset() {
@@ -24,19 +26,34 @@ public class OpenDeskDoor : MonoBehaviour {
 		Cardboard.SDK.VRModeEnabled = !Cardboard.SDK.VRModeEnabled;
 	}
 	
-	public void OpenDoor() {
+	public void OpenDoor(bool setRightDoor=false) {
+		rightDoor = setRightDoor?-1:1;
+		if(isOpening){
+			return;
+		}
 		isOpening = true;
+		openSign = rightDoor*(isOpen ? -1 : 1);
+		isOpen = !isOpen;
 	}
 
 	public void Update() {
-		if (isOpening) {
-			timer += Time.deltaTime;
-			transform.Rotate(0, 0, isOpen*2*Time.deltaTime);
-		}
-		if(timer > 50) {
-			isOpening = false;
-			timer = 0;
-			isOpen = -isOpen;
+		if(isOpening){
+			float rot = openSign*Time.deltaTime*30;
+			currentRotation += rot;
+			transform.Rotate(Vector3.forward*(rot));
+			if(rightDoor == -1?currentRotation < -100:currentRotation > 100){
+				transform.Rotate(Vector3.forward*(rightDoor*100-currentRotation));
+				currentRotation = rightDoor*100;
+				isOpen = true;
+				isOpening = false;
+				openSign = 0;
+			} else if(rightDoor == -1?currentRotation>0:currentRotation < 0){
+				transform.Rotate(Vector3.forward*(-currentRotation));
+				currentRotation = 0;
+				isOpen = false;
+				isOpening = false;
+				openSign = 0;
+			}
 		}
 	}
 }
